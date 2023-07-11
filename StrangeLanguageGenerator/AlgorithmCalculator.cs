a24deee07cabb54a955a74814e50024a650055e2
yoz75 <hui@sosy.uOtchima>
2023-07-11 18:22:34 +0300

    Добавьте файлы проекта.

---
+++ StrangeLanguageGenerator/AlgorithmCalculator.cs
@@ -0,0 +1,64 @@
+﻿
+
+using System;
+using System.Threading.Tasks;
+
+namespace StrangeLanguageGenerator
+{
+    public class AlgorithmCalculator
+        //Привет
+    {
+        /// <summary>
+        /// Calculating algorithm
+        /// </summary>
+        /// <param name="inputData">Text to calculate</param>
+        /// <param name="seed">Algorithm seed, use null if you want to leave random</param>
+        /// <returns></returns>
+        public string Calculate(string inputData, bool isReversedIndex, short wordlength = 2)
+        {
+            Random rand = new Random();
+            string result = string.Empty;
+
+            var index = rand.Next(0, (inputData.Length - 1));
+            result += inputData[index].ToString();
+            result += inputData[index + 1].ToString();
+
+            for (int i = 0; i < inputData.Length - 1; i++)
+            {
+                if (i != index)
+                {
+                    try
+                    {
+                        if (isReversedIndex)
+                        {
+
+                            if (inputData[^i] == result[^i] && inputData[^(i + 1)] == result[^(i + 1)])
+                            {
+                                result += inputData[i + 1];
+                                result += inputData[i + 2];
+
+                            }
+                        }
+                        else
+                        {
+                            if (inputData[i] == result[i] && inputData[i + 1] == result[i + 1])
+                            {
+                                result += inputData[i + 1];
+                                result += inputData[i + 2];
+
+                            }
+                        }
+
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        return result;
+                    }
+                }
+
+            }
+
+            return result;
+        }
+    }
+}
---
+++ StrangeLanguageGenerator/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Windows;
+using System.IO;
+
+
+namespace StrangeLanguageGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private short iteration = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private  void GenerateButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            AlgorithmCalculator algCul = new AlgorithmCalculator();
+            if (iteration % 2 == 0)
+            {
+                 ResultUserText.Text += algCul.Calculate(UserText.Text, isReversedIndex: false, wordlength: Convert.ToInt16(LengthText.Text));
+            }
+            else
+            {
+                ResultUserText.Text += algCul.Calculate(UserText.Text, isReversedIndex: true, wordlength: Convert.ToInt16(LengthText.Text));
+            }
+        
+            iteration++;
+        }
+
+        private void OpenFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.FileName = "Text";
+            dialog.DefaultExt = ".txt";
+            dialog.Filter = "Text documents |*.txt";
+            bool? result = dialog.ShowDialog();
+            if (result.Value)
+            {
+                StreamReader reader = new StreamReader(dialog.FileName);
+
+                UserText.Text = reader.ReadToEnd();
+            }
+        }
+
+        private void SaveFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Microsoft.Win32.SaveFileDialog();
+            dialog.FileName = "Document"; // Default file name
+            dialog.DefaultExt = ".txt"; // Default file extension
+            dialog.Filter = "Text documents (.txt)|*.txt";
+
+            // Show save file dialog box
+            bool? result = dialog.ShowDialog();
+
+            // Process save file dialog box results
+            if (result == true)
+            {
+                // Save document
+                string filename = dialog.FileName;
+                bool isExist = File.Exists(filename);
+                if (isExist)
+                {
+                    File.WriteAllText(filename, ResultUserText.Text);
+                }
+                else
+                {
+                    File.Create(filename);
+                    File.WriteAllText(filename, ResultUserText.Text);
+                }
+            }
+        }
+
+        private void Scb_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            LengthText.Text = Scb.Value.ToString();
+            Scb.Maximum = UserText.Text.Length - 1 ;
+        }
+    }
+}
